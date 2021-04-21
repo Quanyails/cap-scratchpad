52109b4e009933e53ff159d0223af95dc07aee7d
Yijing Chen <yijing@duolingo.com>
2021-04-20 22:29:05 -0400

    Fix ballotMaker not matching on newlines.

--- ballotMaker.js
+++ ballotMaker.js
@@ -1,16 +1,16 @@
 (() => {
     const ballots = Array.from($(".message-body"))
+        // Remove OP
+        .slice(1)
         .map(el => {
             // The part of the post that contains the ballot options is
             // the part of the post up to the first two newlines in that post,
             // or the full post if the user has no comments after the ballot.
             const text = el.innerText.trim();
-            const ballotText = text.match(/(.*)\n\n/);
+            const ballotText = text.match(/(.*?)\n\n/s);
             const ballot = (ballotText ? ballotText[1] : text).split("\n");
             return ballot.map(line => line.trim());
-        })
-        // Remove OP
-        .slice(1);
+        });
 
     const result = "poll://" + ballots.map(ballot => ballot.join(">")).join(",");
 
